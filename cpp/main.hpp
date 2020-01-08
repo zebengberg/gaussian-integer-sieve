9e035839cdda84cb951cca06af3ea2245fa219ed
Zeb Engberg <zebengberg@gmail.com>
2020-01-07 21:39:46 -0700

    restructured project directories

--- cpp/main.hpp
+++
@@ -1,7 +0,0 @@
-#include <vector>
-using namespace std;
-
-vector<pair<long, long>> gPrimes(long);
-vector<pair<long, long>> gPrimesSegment(long, long, long);
-unsigned long gPrimesCount(long);
-unsigned long gPrimesSegmentCount(long, long, long);
