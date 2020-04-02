b5cce0e9cca43db52e21c471bf588a5702ee82f2
Zeb Engberg <zebengberg@gmail.com>
2020-04-02 09:03:23 -0600

    put test into setup.py

--- python/setup.py
+++ python/setup.py
@@ -30,19 +30,35 @@ setup(
     license='MIT',
     ext_modules=cythonize(extensions))
 
-# Rename the shared object file gintsieve.xxxxxx.so
+# Rename the shared object file from gintsieve.xxxxxx.so to gintsieve.so
 for file in os.listdir('.'):
     if file.endswith('.so'):
         os.rename(file, 'gintsieve.so')
         break
 
+
+print('Cleaning up after build...')
+# Cleaning up build directory after the cythonize build
+shutil.rmtree('build/')
+# Removing the cython generated .cpp
+os.remove('src/gintsieve.cpp')
+
+
+print('Testing the module just built...')
+import gintsieve
 try:
-    print('Cleaning up after build...')
-    # Cleaning up build directory after the cythonize build
-    shutil.rmtree('build/')
-    # Removing the cython generated .cpp
-    os.remove('src/gintsieve.cpp')
-except FileNotFoundError:
-    pass
-
-
+    # Values taken from http://oeis.org/A091100
+    assert gintsieve.count_gprimes(10 ** 0) == 0
+    assert gintsieve.count_gprimes(10 ** 1) == 16
+    assert gintsieve.count_gprimes(10 ** 2) == 100
+    assert gintsieve.count_gprimes(10 ** 3) == 668
+    assert gintsieve.count_gprimes(10 ** 4) == 4928
+    assert gintsieve.count_gprimes(10 ** 5) == 38404
+    assert gintsieve.count_gprimes(10 ** 6) == 313752
+    assert gintsieve.count_gprimes(10 ** 7) == 2658344
+    assert gintsieve.count_gprimes(10 ** 8) == 23046512
+    assert gintsieve.count_gprimes(10 ** 9) == 203394764
+except AssertionError:
+    print('Tests failed. Something went very wrong! The module gintsieve is corrupt.\n')
+else:
+    print('Tests passed. Module gintsieve is ready to go.\n')
